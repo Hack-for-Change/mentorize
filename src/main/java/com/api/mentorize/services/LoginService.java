7815cd6f2e487d6de1e244886bbedcc3451f1f08
Thcamposmartins <thcampos_@live.com>
2023-10-04 22:02:46 -0300

    FEAT Adicionando cryptografia para pass

--- src/main/java/com/api/mentorize/services/LoginService.java
+++ src/main/java/com/api/mentorize/services/LoginService.java
@@ -9,19 +9,26 @@ import com.api.mentorize.models.Login;
 import com.api.mentorize.dtos.LoginDTO;
 
 
-
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class LoginService {
+    private static final String AES = "AES";
+    private static final String SECRET_KEY = "mentorize";
     @Autowired
     LoginRepository loginRepository;
 
     public Login save(LoginDTO loginDTO) {
         var login = new Login();
         BeanUtils.copyProperties(loginDTO, login);
+        var pass = encrypt(loginDTO.password());
+        login.setPassword(pass);
         return loginRepository.save(login);
     }
 
@@ -48,6 +55,31 @@ public class LoginService {
 
     public void removeById(UUID id) {
         loginRepository.removeById(id);
+    }
+    public static String encrypt(String password) {
+        try {
+            Key key = new SecretKeySpec(SECRET_KEY.getBytes(), AES);
+            Cipher cipher = Cipher.getInstance(AES);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] encrypted = cipher.doFinal(password.getBytes());
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String decrypt(String encryptedPassword) {
+        try {
+            Key key = new SecretKeySpec(SECRET_KEY.getBytes(), AES);
+            Cipher cipher = Cipher.getInstance(AES);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedPassword));
+            return new String(decrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
